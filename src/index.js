1244f7dc5c3e5c7fde55d47b0f4b4cc31fb4955e
Jose Luis <jose.quispe@vallegrande.edu.pe>
2018-12-05 01:54:31 -0500

    Implementacion de routers, ejs y la carpeta view

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 //Imports
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //Settings
 app.set('port', 3000);
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs');
 
 //routers
 app.get('/', (req, res) => {
-    res.send('Hello word :D');
+    //res.sendFile(path.join(__dirname, '/view/index.html'));
+    res.render('index.ejs', { title: 'first web site' });
+
+    //console.log();
 });
 
 //Listen the Server
